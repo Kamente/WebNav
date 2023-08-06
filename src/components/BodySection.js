2d7704644123a100c81258290473650d53fdbf38
Kamente <justinm48000@gmail.com>
2023-08-06 14:02:03 +0300

    body section

---
+++ src/components/BodySection.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function BodySection() {
+    return (
+        <div className='bodysection-container'>
+            <video src='/public/videos/video-2.mp4' autoPlay loop muted>
+                <h>Adventure Awaits</h>
+            </video>
+        </div>
+    )
+}
+
+export default BodySection
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './Button';
 import './Navbar.css'
@@ -17,8 +17,8 @@ function Navbar() {
         }
     };
 
-    useEffect(()=>{
-         showButton();
+    useEffect(() => {
+        showButton();
     }, []);
 
     window.addEventListener('resize', showButton);
@@ -30,7 +30,7 @@ function Navbar() {
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to="/" className='navbar-logo'>
+                    <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
                         TRVL <i className='fab fa-typo3' />
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
