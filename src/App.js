0c4e45c0699072bc0af55926a2c8d77aac9428fc
Kamente <justinm48000@gmail.com>
2023-08-09 10:02:43 +0300

    styled bodysection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BodySection from "./components/BodySection";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <BodySection />
       <Routes>
         <Route path="/" exact />
       </Routes>
--- src/components/BodySection.js
+++ src/components/BodySection.js
@@ -1,12 +1,20 @@
 import React from 'react'
-
+import { Button } from './Button';
+import './BodySection.css'
+import "./Button.css"
 
 function BodySection() {
     return (
         <div className='bodysection-container'>
-            <video src='/public/videos/video-2.mp4' autoPlay loop muted>
-                <h>Adventure Awaits</h>
-            </video>
+            <video src='/public/videos/video-2.mp4' autoPlay loop muted></video>
+            <h2>Ready for Wonder Land ?</h2>
+            <div className='body-btns'>
+                <Button className='btn'
+                    buttonStyle='btn--primary'
+                    buttonSize='btn--large'>
+                    Get Started
+                </Button>
+            </div>
         </div>
     )
 }
